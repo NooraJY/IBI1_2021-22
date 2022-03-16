9c6ca7abaa2ef12b1474c860ad0e9d44b84748fc
Chen Jiayao <jiayaoc.21@intl.zju.edu.cn>
2022-03-16 21:43:36 +0800

    calculate the triangles

---
+++ Practical5/triangle.py
@@ -0,0 +1,7 @@
+n=0 #n represents the triangle number of dots in each side
+for i in range(1,11):
+#i goes up one everytime it cycle once
+	n = n+i
+#The value of n has changed in the circulation.
+	print('When the triangle number is',i,',there are',n,'dots' )
+#print the results
