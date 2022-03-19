b69e6089a1e29689cdab065c50e20978d6c81ec2
NooraJY <101187205+NooraJY@users.noreply.github.com>
2022-03-19 16:06:11 +0800

    add some pseudocode and str()

--- Practical5/pizza_party.py
+++ Practical5/pizza_party.py
@@ -5,7 +5,8 @@ while p < 64:
 	p = (n ** 2 + n + 2)/2
 	print('The number of cuts is', n,'and the pizza can be cut into',p,'pieces')
 #one cycle means one cut is added
-	if p >= 64:
-		print('Now we have enough pieces of pizza for everyone in class.')
-		break
-#The pieces of pizza have exceeded 64 so the programme stops.
+#The pieces of pizza have exceeded 64 so the programme stops after 11 cut.
+str(n)#change the number n into a string
+str(p)#change the number p into a string
+print('we have cut',n,'times and we get',p,'pieces of pizza.Now we have enough pieces of pizza for everyone in class.')
+#print out the results
