79b3da0aa005b9bb065498b7184595521304158a
Chen Jiayao <jiayaoc.21@intl.zju.edu.cn>
2022-03-23 14:31:57 +0800

    task2

---
+++ Practical6/list manipulation.py
@@ -0,0 +1,21 @@
+marks=[45,36,86,57,53,92,65,45]
+marks.sort()
+#sort the list and change the initial order in the list
+print(marks)
+
+import numpy as np
+import matplotlib.pyplot as plt
+#import the module numpy and matplotlib
+score = np.array(marks)
+#set the value in the boxplot from the 'marks' list
+plt.boxplot(score,patch_artist=True)
+plt.title('list manipulation')
+plt.show()
+
+average_mark=sum(marks)/len(marks)
+#calculate the average of numbers in the 'marks' list
+if average_mark >= 60:
+	print('the average mark is',average_mark,'which is higher than the pass mark of 60%. Rob can pass the ICA.')
+else:
+	print('the average mark is',average_mark,'which is lower than the pass mark of 60%. Rob has failed the ICA.')
+#print differnt results in the pass and fail situations
