eebee5094798de34cbdcda1ec99c710193485489
NooraJY <101187205+NooraJY@users.noreply.github.com>
2022-05-05 23:24:39 +0800

    upload Practical 11

---
+++ Practical11/Alignment.py
@@ -0,0 +1,89 @@
+import re
+seq1 = open(r'DLX5_human.fa','r')
+seq2 = open(r'DLX5_mouse.fa','r')
+seq3 = open(r'RandomSeq.fa')
+
+#get the DNA sequence and store them in the different lists
+seq_list1=[]
+for line in seq1:
+    if not line.startswith('>'):
+        seq_list1.extend(line)
+seq_list1.remove('\n')#combine the lines into one line
+
+seq_list2=[]
+for line in seq2:
+    if not line.startswith('>'):
+        seq_list2.extend(line)
+seq_list2.remove('\n')
+
+seq_list3=[]
+for line in seq3:
+    if not line.startswith('>'):
+        seq_list3.extend(line)
+seq_list3.remove('\n')
+
+#set the standard to calculate the score
+amino_acid=['A','R','N','D','C','Q','E','G','H','I','L','K','M','F','P','S','T','W','Y','V','B','Z','X','*']
+BLOSUM62=[
+[ 4, -1, -2, -2,  0, -1, -1,  0, -2, -1, -1, -1, -1, -2, -1,  1,  0, -3, -2,  0, -2, -1,  0, -4],
+[-1,  5,  0, -2, -3,  1,  0, -2,  0, -3, -2,  2, -1, -3, -2, -1, -1, -3, -2, -3, -1,  0, -1, -4],
+[-2,  0,  6,  1, -3,  0,  0,  0,  1, -3, -3,  0, -2, -3, -2,  1,  0, -4, -2, -3,  3,  0, -1, -4],
+[-2, -2,  1,  6, -3,  0,  2, -1, -1, -3, -4, -1, -3, -3, -1,  0, -1, -4, -3, -3,  4,  1, -1, -4],
+[ 0, -3, -3, -3,  9, -3, -4, -3, -3, -1, -1, -3, -1, -2, -3, -1, -1, -2, -2, -1, -3, -3, -2, -4],
+[-1,  1,  0,  0, -3,  5,  2, -2,  0, -3, -2,  1,  0, -3, -1,  0, -1, -2, -1, -2,  0,  3, -1, -4],
+[-1,  0,  0,  2, -4,  2,  5, -2,  0, -3, -3,  1, -2, -3, -1,  0, -1, -3, -2, -2,  1,  4, -1, -4],
+[ 0, -2,  0, -1, -3, -2, -2,  6, -2, -4, -4, -2, -3, -3, -2,  0, -2, -2, -3, -3, -1, -2, -1, -4],
+[-2,  0,  1, -1, -3,  0,  0, -2,  8, -3, -3, -1, -2, -1, -2, -1, -2, -2,  2, -3,  0,  0, -1, -4],
+[-1, -3, -3, -3, -1, -3, -3, -4, -3,  4,  2, -3,  1,  0, -3, -2, -1, -3, -1,  3, -3, -3, -1, -4],
+[-1, -2, -3, -4, -1, -2, -3, -4, -3,  2,  4, -2,  2,  0, -3, -2, -1, -2, -1,  1, -4, -3, -1, -4],
+[-1,  2,  0, -1, -3,  1,  1, -2, -1, -3, -2,  5, -1, -3, -1,  0, -1, -3, -2, -2,  0,  1, -1, -4],
+[-1, -1, -2, -3, -1,  0, -2, -3, -2,  1,  2, -1,  5,  0, -2, -1, -1, -1, -1,  1, -3, -1, -1, -4],
+[-2, -3, -3, -3, -2, -3, -3, -3, -1,  0,  0, -3,  0,  6, -4, -2, -2,  1,  3, -1, -3, -3, -1, -4],
+[-1, -2, -2, -1, -3, -1, -1, -2, -2, -3, -3, -1, -2, -4,  7, -1, -1, -4, -3, -2, -2, -1, -2, -4],
+[ 1, -1,  1,  0, -1,  0,  0,  0, -1, -2, -2,  0, -1, -2, -1,  4,  1, -3, -2, -2,  0,  0,  0, -4],
+[ 0, -1,  0, -1, -1, -1, -1, -2, -2, -1, -1, -1, -1, -2, -1,  1,  5, -2, -2,  0, -1, -1,  0, -4],
+[-3, -3, -4, -4, -2, -2, -3, -2, -2, -3, -2, -3, -1,  1, -4, -3, -2, 11,  2, -3, -4, -3, -2, -4],
+[-2, -2, -2, -3, -2, -1, -2, -3,  2, -1, -1, -2, -1,  3, -3, -2, -2,  2,  7, -1, -3, -2, -1, -4],
+[ 0, -3, -3, -3, -1, -2, -2, -3, -3,  3,  1, -2,  1, -1, -2, -2,  0, -3, -1,  4, -3, -2, -1, -4],
+[-2, -1,  3,  4, -3,  0,  1, -1,  0, -3, -4,  0, -3, -3, -2,  0, -1, -4, -3, -3,  4,  1, -1, -4],
+[-1,  0,  0,  1, -3,  3,  4, -2,  0, -3, -3,  1, -1, -3, -1,  0, -1, -3, -2, -2,  1,  4, -1, -4],
+[ 0, -1, -1, -1, -2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -2,  0,  0, -2, -1, -1, -1, -1, -1, -4],
+[-4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4, -4,  1],
+]
+
+#a and b represent the sequences that we need to compare
+#define a function to calculate the alignment score
+def compare(a,b):
+    score = 0#set initial score as zero
+    for i in range(len(a)):
+        name1 = amino_acid.index(a[i])
+        name2 = amino_acid.index(b[i])
+        score = score + BLOSUM62[name1][name2]
+        #compare the amino acid that in the same position in the two sequences
+    print('The alignment score is:',score)
+    return score
+
+def calculate_score(a,b):
+    edit_distance = 0 #set initial distance as zero
+    for i in range(len(a)): #compare each amino acid
+        if  a[i]!=b[i]:		
+            edit_distance += 1 #add a score 1 if amino acids are different
+        percentage = edit_distance/len(a)
+    print('The percentage of identical amino acids is:',100*(1-percentage),'%')
+    return percentage
+
+#use the functions that I have defined above
+compare(seq_list1,seq_list2)
+calculate_score(seq_list1,seq_list2)
+compare(seq_list1,seq_list3)
+calculate_score(seq_list1,seq_list3)
+compare(seq_list2,seq_list3)
+calculate_score(seq_list2,seq_list3)
+
+seq1.close()
+seq2.close()
+seq3.close()
+
+
+
+
